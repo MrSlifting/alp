747ea33e54a5d20d11fa3588b33c820b17955d88
OLDLP <53021568+JincHeSher@users.noreply.github.com>
2020-10-15 04:55:56 -0400

    添加搜索功能

--- src/app/search-barcode/search-barcode.page.ts
+++ src/app/search-barcode/search-barcode.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import products from '../../assets/data/products.json';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-search-barcode',
@@ -8,13 +9,35 @@ import products from '../../assets/data/products.json';
 })
 export class SearchBarcodePage implements OnInit {
 
-  constructor() { }
+  searchKeywd: string;
+  searchType: string;
+  results: any;
+
+  constructor(public alertController: AlertController) { }
+
 
   ngOnInit() {
   }
 
-  getProduct(){
-    
+  getProduct() {
+    switch (this.searchType) {
+      case "zhName": this.results = products.filter(key => key.zhName.includes(this.searchKeywd)); break;
+      case "enName": this.results = products.filter(key => key.enName.includes(this.searchKeywd)); break;
+      case "department": this.results = products.filter(key => key.department.includes(this.searchKeywd)); break;
+      default: this.presentAlert();
+    }
+  }
+
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: '提示',
+      subHeader: '没有选择搜索类型',
+      message: '请输入搜索类型',
+      buttons: ['确认']
+    });
+    await alert.present();
   }
 
 }
