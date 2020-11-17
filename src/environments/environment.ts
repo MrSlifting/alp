2df624d21bcfc30c2a9e6ac80d890941ce221989
JincHeSher <53021568+JincHeSher@users.noreply.github.com>
2020-11-16 22:10:43 -0500

    firebase support and google forms

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDTu5G_R5zr-xWqsCUo6LvcFFDn26a0G8Y",
+    authDomain: "alp-app-c759d.firebaseapp.com",
+    databaseURL: "https://alp-app-c759d.firebaseio.com",
+    projectId: "alp-app-c759d",
+    storageBucket: "alp-app-c759d.appspot.com",
+    messagingSenderId: "192094753783",
+    appId: "1:192094753783:web:a18247b14cebb71aa554f2",
+    measurementId: "G-5HEVZZ27HC"
+  }
 };
 
 /*
